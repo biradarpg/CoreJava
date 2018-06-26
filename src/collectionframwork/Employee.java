22bb66c3617bfc0bdcaf32f043a0e826a8129d96
praveen <praveen@graspio.com>
2018-06-26 23:28:15 +0530

    # WARNING: head commit changed in the me

---
+++ src/collectionframwork/Employee.java
@@ -0,0 +1,13 @@
+package collectionframwork;
+
+public class Employee {
+	String name;
+	String dept;
+	int age;
+	public Employee(String name,int age,String dept){
+		this.name=name;
+		this.age=age;
+		this.dept=dept;
+	}
+
+}
